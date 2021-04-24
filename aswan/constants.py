543b0ea0923b08c6e0728d63be1ebd42bd53c347
Endre Márk Borza <endremborza@gmail.com>
2021-04-24 20:25:54 +0200

    time constants

--- aswan/constants.py
+++ aswan/constants.py
@@ -20,6 +20,12 @@ CONFIG_PATH.mkdir(exist_ok=True)
 
 url_root_regex = re.compile(r"^(?://|[^/]+)*/")
 
+ONE_MINUTE = 60
+ONE_HOUR = ONE_MINUTE * 60
+ONE_DAY = ONE_HOUR * 24
+ONE_WEEK = ONE_DAY * 7
+ONE_YEAR = ONE_DAY * 365
+
 
 class Statuses:
 
