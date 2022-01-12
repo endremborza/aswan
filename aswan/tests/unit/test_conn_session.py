b5e498ae3bdca26cff828dea623a121ecb953016
Endre Márk Borza <endremborza@gmail.com>
2022-01-12 21:43:46 +0100

    cs init fix

--- aswan/tests/unit/test_conn_session.py
+++ aswan/tests/unit/test_conn_session.py
@@ -8,7 +8,7 @@ from aswan.tests.godel_src.app import test_app_default_address
 def test_godel_defaults(tmp_path, godel_test_app):
 
     ostr = get_object_store(str(tmp_path))
-    cm = ConnectionSession([])
+    cm = ConnectionSession()
 
     url = f"{test_app_default_address}/test_page/Axiom.html"
     task = HandlingTask(UrlHandler(), url, ostr)
