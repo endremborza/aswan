feabc43ce10b0cd4278b99021893c7ad2065a754
Endre Márk Borza <endremborza@gmail.com>
2022-11-14 10:58:50 +0100

    expose types

--- aswan/__init__.py
+++ aswan/__init__.py
@@ -2,13 +2,14 @@
 """Data collection manager"""
 from .connection_session import ConnectionSession
 from .constants import ONE_DAY, ONE_HOUR, ONE_MINUTE, ONE_WEEK, ONE_YEAR, Statuses
-from .depot import AswanDepot
+from .depot import AswanDepot, ParsedCollectionEvent
 from .exceptions import BrokenSessionError, ConnectionError
 from .object_store import ObjectStore
 from .project import Project
 from .security import ProxyAuth, ProxyBase
 from .simplified_functions import get_json, get_soup, run_simple_project
 from .url_handler import (
+    ANY_HANDLER_T,
     BrowserHandler,
     BrowserJsonHandler,
     BrowserSoupHandler,
